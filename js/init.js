cc36da925bd68f1973851d543e4379c866b1c181
lupalianoferre <lpaliano2013@gmail.com>
2025-10-07 11:53:48 -0300

    my-profile.html
    Direccionar la Pestaña "Usuario" a my-profile.html
    Cambiar estilo del cursor del mouse

--- js/init.js
+++ js/init.js
@@ -38,4 +38,52 @@ let getJSONData = function(url){
         hideSpinner();
         return result;
     });
+<<<<<<< Updated upstream
 }
+=======
+}
+
+// Función para mostrar el nombre de usuario en la barra de navegación
+function mostrarNombreUsuario() {
+    // Obtener datos de sesión del almacenamiento local
+    const session = localStorage.getItem('userSession');
+    // Obtener elemento donde mostrar el nombre de usuario
+    const usernameElement = document.getElementById('navusername');
+    // Obtener enlace de inicio de sesión
+    const loginLink = document.querySelector('.nav-link[href="login.html"]');
+  
+    // Si hay sesión y elemento de usuario existe
+    if (session && usernameElement) {
+        // Parsear datos de usuario y mostrar nombre
+        const userData = JSON.parse(session);
+        usernameElement.textContent = userData.usuario;
+        usernameElement.style.display = '';
+        // remover enlace de login si existe
+        if (loginLink) {
+            loginLink.remove();
+        }
+    } else if (usernameElement) {
+        // Si no hay sesión, remueve nombre de usuario
+        usernameElement.remove(); 
+    }
+}
+// 7/10/25: Evento para que al lcickear el nombre de usuario en el nav, redirija al Perfil
+document.getElementById('navusername').addEventListener('click', function(e) {
+    e.preventDefault(); // Prevenir la acción por defecto del enlace
+    window.location.href = 'my-profile.html'; // Redirige a la página del perfil
+  });
+
+/* Código para que redireccione a Login o Index según corresponda */
+document.addEventListener('DOMContentLoaded', function() {
+    const session = localStorage.getItem('userSession');
+    if (session && window.location.pathname.includes(`login.html`)){
+      window.location.href =`index.html`;
+    }
+    else if
+      (!session && !window.location.pathname.includes(`login.html`)){
+      window.location.href =`login.html`;
+    }
+    
+    mostrarNombreUsuario();
+});
+>>>>>>> Stashed changes
